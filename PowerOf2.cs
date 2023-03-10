006d67cda714303ac6c556902cea9a4796d0dcca
samirasingh19 <samira.singh021@gmail.com>
2023-03-10 21:13:38 +0530

    PowerOf2

---
+++ PowerOf2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1Assignment
+{
+    internal class PowerOf2
+    {
+        public static void Power2()
+        {
+
+            int value = 2;
+            for (int power = 0; power <= 32; power++)
+                Console.WriteLine($"{value}^{power} = {(long)Math.Pow(value, power): N0}");
+
+
+        }
+    }
+}
